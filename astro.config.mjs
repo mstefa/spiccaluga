dc8def060176ecc4cc362b10bf8c3aec5969ad9b
Matias Stefanutti <mstefanutti24@gmail.com>
2025-03-31 08:58:50 +0200

    feat: add action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://mstefa.github.io',
+  base: '/spiccaluga',
 });
